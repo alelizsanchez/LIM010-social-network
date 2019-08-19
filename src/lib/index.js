4ec0e98e6d9b2f92a5a643fe8eba102d42e665ca
Alelizsanchez <Alelizsanchez@gmail.com>
2019-08-19 09:34:42 -0500

    subiendo post

--- src/lib/index.js
+++ src/lib/index.js
@@ -66,6 +66,57 @@ const guardarDatos = () => {
   arrEmail = JSON.parse(localStorage.getItem('primer-correo'));
 
 }
+/* post */
+/**
+ * Ingresar datos mediante un formulario
+ */
+function manejarDatosPost(){
+
+  // Detectar el click el botñón "guardar"
+  document.getElementById("guardar-post").addEventListener("click", () => {
+      
+      // Recupera el texto de la caja
+      const texto = document.getElementById("texto-post").value;
+      guardarPost(texto);
+  });
+}
+
+/**
+* Guarda un post en localstorage
+*/
+function guardarPost(texto){
+
+  const post = {
+      texto: texto
+  };
+
+  // Guardamos el dato en localStorage
+  localStorage.setItem('post', JSON.stringify(post));
+
+  // Invocamos al método para mostrar el post
+  mostrarPost();
+  
+}
+
+/**
+* Muestra una post que ya existe en localStorage en pantalla
+*/
+function mostrarPost(){
+  const post = JSON.parse(localStorage.getItem('post'));
+  console.log(post);
+
+  //console.log('Post recuperado:', post.texto);
+  document.getElementById("post-recuperado").innerHTML = post.texto;
+
+  // Retiro la clase que lo oculta
+  // Post__mostrar oculto 
+  document.getElementById("caja-post").classList.remove('oculto');
+}
+
+
+// Inicia los métodos
+manejarDatosPost();
+/*  fin post */
 
 
 
