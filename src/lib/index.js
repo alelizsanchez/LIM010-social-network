b6e74ebf74791f8f77af8875017cfbf2cc2bfa49
Gabby97 <gabbacvdor@gmail.com>
2019-08-21 09:02:05 -0500

    actualizando login

--- src/lib/index.js
+++ src/lib/index.js
@@ -30,11 +30,6 @@ const guardarDatos = () => {
   const contraseña = document.getElementById('pw').value;
   const validaCorreo = /\w+@\w+\.+[a-z]/;
   
-  //declarando variables para el login
-  const formRegistro = document.getElementById('registro');
-  const formLogin = document.getElementById('login');
-  const ingrsarr = document.getElementById('ingresarr');
-  const btnIngresar = document.getElementById('ingresar');
 
   arrNombres.push(nombreUsuario);
   arrEmail.push(correoUsuario);
@@ -57,51 +52,9 @@ const guardarDatos = () => {
   if (contraseña.length < 6 || contraseña.length > 8) {
     errorPw.innerHTML = 'Contraseña muy corta, por favor usa de 6 a 8 caracteres';
     return false;
-        
-    formLogin.classList.remove('hide');
-  formLogin.classList.add('block');
-  const emailIngreso = document.getElementById('email-login').value;
-  const pwIngreso = document.getElementById('pw-login').value;
-  if (emailIngreso !== correoUsuario) {
-    alert('El coreo no exixte, por favor regístrese');
-    return false;
-  }
-
   }
 }
 btnRegistro.addEventListener('click', guardarDatos);
-
-
-
-
-//  btnRegistro.addEventListener('click', (guardarDatos) => {
-//    for (let i = 0; i<= guardarDatos.length; i++){
-
-//    }
-//   const nombreUsuario = document.getElementById('nombre').value;
-//   const correoUsuario = document.getElementById('email').value;
-//   const contraseña = document.getElementById('pw').value;
-//   const validaCorreo = /\w+@\w+\.+[a-z]/;
-
-//   localStorage.setItem('name', nombreUsuario);
-//   localStorage.setItem('email', correoUsuario);
-
-//   if (nombreUsuario === '' || correoUsuario === '' || contraseña === '') {
-//     todoCampo.innerHTML = 'Todos los campos estan vacíos';
-//     return false;
-//   }
-//   else if (!validaCorreo.test(correoUsuario)) {
-//     validarEmail.innerHTML = 'El correo no es válido';
-//     return false;
-//   }
-//   if (contraseña.length < 6 || contraseña.length > 8) {
-//     errorPw.innerHTML = 'Contraseña muy corta, por favor usa de 6 a 8 caracteres';
-//     return false;
-
-//   }
-//   localStorage.getItem('name');
-//   localStorage.getItem('email');
-// })
 
 const formRegistro = document.getElementById('registro');
 const formLogin = document.getElementById('login');
